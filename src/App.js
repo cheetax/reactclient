0ea8d4df22c423e75a0daecb7ea5c9fa0f2d26f8
Дмитрий Гребенев <dmitriy.grebenev@gmail.com>
2018-05-20 23:54:54 +0300

    new

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  activateLasers = () => {
+    console.log('test')
+  }
   render() {
     return (
       <div className="App">
@@ -11,8 +14,12 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started! edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.activateLasers.bind(this)}>
+          Activate Lasers
+        </button>
+        
       </div>
     );
   }
