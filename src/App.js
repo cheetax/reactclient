ff80fb8cba400b317ee90feac5c626f93a3b8bbd
Дмитрий Гребенев <dmitriy.grebenev@gmail.com>
2018-05-22 00:33:22 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CookiesProvider } from 'react-cookie'
 
 class App extends Component {
   activateLasers = () => {
@@ -8,22 +9,26 @@ class App extends Component {
     this.win = window;
     this.win.location = 'http://localhost:3001/login';
     console.log(this.win.location)
+    get
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          LOGIN:
+      <CookiesProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            LOGIN:
         </p>
-        <button onClick={this.activateLasers.bind(this)}>
-          Login
+          <button onClick={this.activateLasers.bind(this)}>
+            Login
         </button>
 
-      </div>
+        </div>
+      </CookiesProvider>
+
     );
   }
 }
