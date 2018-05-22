aa12712e21a8c2fa913d3c4556f3628745dcbb20
cheetax <dmitriy.grebenev@gmail.com>
2018-05-22 17:07:12 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,36 +1,80 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { CookiesProvider } from 'react-cookie'
+import { withCookies, Cookies } from 'react-cookie'
+import cookie from 'react-cookie'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: null
+    }
+  }
+
+  componentWillMount() {
+    const { cookies } = this.props;
+    var id = cookies.get('id');
+    this.setState({
+      id: id || null
+    });
+  }
+
+  componentDidUpdate() {
+    const { cookies } = this.props;
+    var login = cookies.get('login');
+
+  }
+
   activateLasers = () => {
     console.log(window.location)
     this.win = window;
     this.win.location = 'http://localhost:3001/login';
     console.log(this.win.location)
-    get
   }
-  render() {
-    return (
-      <CookiesProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            LOGIN:
-        </p>
-          <button onClick={this.activateLasers.bind(this)}>
-            Login
-        </button>
 
-        </div>
-      </CookiesProvider>
+  getUser = () => {
+    const { cookies } = this.props;
+   
+    fetch('http://localhost:3001/users',
+      {
+        method: 'POST',
+        body: JSON.stringify({id: this.state.id}),
+        //credentials: 'same-origin',
+        withCredentials: true,
+        headers: {
+          'Content-Type': "application/json",
+          'Access-Control-Allow-Origin': 'http://localhost:3000'
+        }
+      }).then(res => {
+        console.log(res)
+      })
+
+  }
+
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          LOGIN: {this.state.login}
+        </p>
+        <button onClick={this.activateLasers.bind(this)}>
+          Login
+        </button>
+        <button onClick={this.getUser.bind(this)}>
+          Get Users
+        </button>
+      </div>
 
     );
   }
 }
 
-export default App;
+export default withCookies(App);
