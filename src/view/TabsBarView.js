8dbf82f43418ab2405d9eb80f398f31d3daf83a4
Дмитрий Гребенев <dmitriy.grebenev@gmail.com>
2018-05-28 00:32:38 +0300

    new

--- src/view/TabsBarView.js
+++ src/view/TabsBarView.js
@@ -4,12 +4,7 @@ import { MDCTabBar, MDCTab } from '@material/tabs'
 const TabsBarView = () => {
 
     return (
-        <nav id="basic-tab-bar" class="mdc-tab-bar">
-            <a class="mdc-tab mdc-tab--active" href="#one">Home</a>
-            <a class="mdc-tab" href="#two">Merchandise</a>
-            <a class="mdc-tab" href="#three">About Us</a>
-            <span class="mdc-tab-bar__indicator"></span>
-        </nav>
+        <div> test </div>
     )
 
 }
