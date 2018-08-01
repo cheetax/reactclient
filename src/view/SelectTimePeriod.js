572cdd7f2b7c509e3b009931246009dd847f3188
cheetax <dmitriy.grebenev@gmail.com>
2018-08-01 23:55:09 +0300

    new

--- src/view/SelectTimePeriod.js
+++ src/view/SelectTimePeriod.js
@@ -17,8 +17,9 @@ class SelectTimePeriod extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      dataFrom: props.dataFrom || moment().startOf('day'),
+      dataFrom: props.dataFrom || moment().startOf('month'),
       dataTo: props.dataFrom || moment().endOf('month'),
+      resultType: null,
     }
   }
 
@@ -30,22 +31,61 @@ class SelectTimePeriod extends Component {
 
   }
 
-  getPeriodToString = () => {
-    var result = (this.state.dataFrom.format('L') + ' - ' + this.state.dataTo.format('L'));
-    if (this.state.dataFrom.valueOf() ===  moment(this.state.dataTo).startOf('day').valueOf()) result = this.state.dataFrom.format('L');
-    if (moment(this.state.dataFrom).date() === 1) { 
-      switch (this.state.dataTo.valueOf()) {
-        case moment(this.state.dataTo).endOf('month').valueOf():
-          result = this.state.dataFrom.format('MMMM YYYY');
-          break;
-            
-        //=== moment(this.state.dataTo).startOf('month').valueOf()) result = this.state.dataFrom.format('L');
-      }
+  _upPeriod = () => {
+    if (this.state.resultType) {
+      var dataFrom = this.state.dataFrom.add(1, this.state.resultType)
+      var dataTo = this.state.dataTo.add(1, this.state.resultType)
+      this.setState({
+        dataFrom: dataFrom,
+        dataTo: dataTo,
+      })
+    }
+  }
+
+  _downPeriod = () => {
+    if (this.state.resultType) {
+      var dataFrom = this.state.dataFrom.subtract(1, this.state.resultType)
+      var dataTo = this.state.dataTo.subtract(1, this.state.resultType)
+      this.setState({
+        dataFrom: dataFrom,
+        dataTo: dataTo,
+      })
     }
 
-    //if ()
+  }
+
+  getPeriodToString = () => {
+
+    var result = (this.state.dataFrom.format('L') + ' - ' + this.state.dataTo.format('L'));
+    var resultType = null;
+    //var qu = moment(this.state.dataFrom).startOf('quarter');
+    if (this.state.dataFrom.valueOf() === moment(this.state.dataTo).startOf('day').valueOf()) {
+      result = this.state.dataFrom.format('L');
+      resultType = 'days';
+    }
+    else if (moment(this.state.dataFrom).startOf('year').valueOf() === this.state.dataFrom.valueOf() &&
+      moment(this.state.dataFrom).endOf('year').valueOf() === this.state.dataTo.valueOf()) {
+      result = moment(this.state.dataFrom).year() + ' год';
+      resultType = 'years';
+    }
+    else if (moment(this.state.dataFrom).startOf('month').valueOf() === this.state.dataFrom.valueOf() &&
+      moment(this.state.dataFrom).endOf('month').valueOf() === this.state.dataTo.valueOf()) {
+      result = this.state.dataFrom.format('MMMM YYYY');
+      resultType = 'months';
+    }
+    else if (moment(this.state.dataFrom).startOf('quarter').valueOf() === this.state.dataFrom.valueOf() &&
+      moment(this.state.dataFrom).endOf('quarter').valueOf() === this.state.dataTo.valueOf()) {
+      result = this.state.dataFrom.format('Qo') + ' квартал';
+      resultType = 'quarters';
+    }   //if ()
+    console.log(moment(this.state.dataFrom).endOf('month').format())
+    if (this.state.resultType !== resultType) {
+      this.setState({
+        resultType: resultType,
+      })
+    }
     //var endMonth = this.state.dataFrom.endOf('month');
-    return result
+    return result;
   }
 
 
@@ -54,16 +94,16 @@ class SelectTimePeriod extends Component {
     return (
       <div className="flex-row">
         <a className='waves-effect waves-light btn-floating btn'
-          onClick={() => { }} >
+          onClick={() => this._downPeriod()} >
           <i className="material-icons"  >keyboard_arrow_left</i>
         </a>
         <a className='waves-effect waves-light btn-raised-floating btn'
           style={{ margin: '0 10px' }}
-          onClick={() => { }} >
+          onClick={() => this._upPeriod()} >
           <div>{this.getPeriodToString()}</div>
         </a>
         <a className='waves-effect waves-light btn-floating btn'
-          onClick={() => { }} >
+          onClick={() => this._upPeriod()} >
           <i className="material-icons"  >keyboard_arrow_right</i>
         </a>
 
